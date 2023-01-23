55374f8d7bf61fa679ae13858d71f13dcd0b8274
expeditus303 <ricardotonietto@gmail.com>
2023-01-23 16:25:28 -0300

    delete registry

--- src/app.js
+++ src/app.js
@@ -21,193 +21,295 @@ const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`);
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 const mongoClient = new MongoClient(DATABASE_URL);
 let db;
 
 try {
-    await mongoClient.connect();
-    db = mongoClient.db();
-    console.log("Data base is connected");
+  await mongoClient.connect();
+  db = mongoClient.db();
+  console.log("Data base is connected");
 } catch (error) {
-    console.log("Can't connect to data base");
+  console.log("Can't connect to data base");
 }
 
 app.get("/registry", async (req, res) => {
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
 
-    if (!token) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401);
 
-    const registrySchema = joi.object({
-        token: joi.string().required(),
-    });
+  const registrySchema = joi.object({
+    token: joi.string().required(),
+  });
 
-    const validation = registrySchema.validate({token});
+  const validation = registrySchema.validate({ token });
 
-    if (validation.error) return res.status(400).send(validation.error.message);
+  if (validation.error) return res.status(400).send(validation.error.message);
 
-    try {
-        const tokenExists = await db.collection(SESSIONS).findOne({ token });
+  try {
+    const tokenExists = await db.collection(SESSIONS).findOne({ token });
 
-        if (!tokenExists) return res.sendStatus(401);
+    if (!tokenExists) return res.sendStatus(401);
 
-        const userRegistry = await db.collection(REGISTRY).find({ userId: tokenExists.user_id }).toArray()
+    const userRegistry = await db
+      .collection(REGISTRY)
+      .find({ userId: tokenExists.user_id })
+      .toArray();
 
-        res.status(200).send(userRegistry)
+    if (userRegistry.length === 0)
+      return res.send([tokenExists.name]).status(200);
 
-    } catch (error) {
-        res.send(error)
-    }
+    const userSubtotal = await db
+      .collection("subtotal")
+      .findOne({ userId: tokenExists.user_id });
 
-})
+    const userData = [[tokenExists.name], [...userRegistry], [userSubtotal]];
+
+    res.status(200).send(userData);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 app.post("/sign-in", async (req, res) => {
-    const { email } = req.body;
-    const { password } = req.headers;
+  const { email } = req.body;
+  const { password } = req.headers;
 
-    const userSchema = joi.object({
-        email: joi.string().email().required(),
-        password: joi.string().required(),
-    });
+  const userSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
 
-    const validation = userSchema.validate({ email, password });
+  const validation = userSchema.validate({ email, password });
 
-    if (validation.error) return res.sendStatus(400);
+  if (validation.error) return res.sendStatus(400);
 
-    try {
-        const emailExists = await db.collection(USERS).findOne({ email });
+  try {
+    const emailExists = await db.collection(USERS).findOne({ email });
 
-        if (!emailExists)
-            return res
-                .status(400)
-                .send("You have entered an invalid username or password");
+    if (!emailExists)
+      return res
+        .status(400)
+        .send("You have entered an invalid username or password");
 
-        const hashPassword = bcrypt.compareSync(password, emailExists.password);
+    const hashPassword = bcrypt.compareSync(password, emailExists.password);
 
-        if (!hashPassword)
-            return res
-                .status(400)
-                .send("You have entered an invalid username or password");
+    if (!hashPassword)
+      return res
+        .status(400)
+        .send("You have entered an invalid username or password");
 
-        const token = uuid();
+    const token = uuid();
 
-        await db.collection(SESSIONS).insertOne({ user_id: emailExists._id, token });
+    await db
+      .collection(SESSIONS)
+      .insertOne({ user_id: emailExists._id, name: emailExists.name, token });
 
-        return res.status(200).send(token);
-    } catch (error) { }
+    return res.status(200).send({ token });
+  } catch (error) {}
 });
 
 app.post("/sign-up", async (req, res) => {
-    const { name, email } = req.body;
-    const { password, confirmpassword } = req.headers;
+  const { name, email } = req.body;
+  const { password, confirmpassword } = req.headers;
 
-    const userSchema = joi.object({
-        name: joi.string().min(2).required(),
-        email: joi.string().email().required(),
-        password: joi
-            .string()
-            .min(8)
-            .regex(/^(?=.*[A-Z])(?=.*[a-z])/)
-            .required()
-            .messages({
-                "string.pattern.base":
-                    "password must have at least one uppercase and one lowercase letter",
-            }),
-        confirmpassword: joi
-            .string()
-            .valid(joi.ref("password"))
-            .required()
-            .messages({ "any.only": "passwords do not match" }),
-    });
+  const userSchema = joi.object({
+    name: joi.string().min(2).required(),
+    email: joi.string().email().required(),
+    password: joi
+      .string()
+      .min(8)
+      .regex(/^(?=.*[A-Z])(?=.*[a-z])/)
+      .required()
+      .messages({
+        "string.pattern.base":
+          "password must have at least one uppercase and one lowercase letter",
+      }),
+    confirmpassword: joi
+      .string()
+      .valid(joi.ref("password"))
+      .required()
+      .messages({ "any.only": "passwords do not match" }),
+  });
 
-    const validation = userSchema.validate({
-        name,
-        email,
-        password,
-        confirmpassword,
-    });
+  const validation = userSchema.validate({
+    name,
+    email,
+    password,
+    confirmpassword,
+  });
 
-    if (validation.error) return res.sendStatus(400);
+  if (validation.error)
+    return res.status(400).send(validation.error.details[0].message);
 
-    const passwordHashed = bcrypt.hashSync(password, 10);
+  const passwordHashed = bcrypt.hashSync(password, 10);
 
-    try {
-        const emailExists = await db.collection(USERS).findOne({ email });
+  try {
+    const emailExists = await db.collection(USERS).findOne({ email });
 
-        if (emailExists) return res.status(409).send("email is already in use");
+    if (emailExists) return res.status(409).send("email is already in use");
 
-        await db
-            .collection(USERS)
-            .insertOne({ name, email, password: passwordHashed });
+    await db
+      .collection(USERS)
+      .insertOne({ name, email, password: passwordHashed });
 
-        return res.send("ok");
-    } catch (error) {
-        res.sendStatus(500);
-    }
+    return res.send("ok");
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
 app.post("/new-transaction", async (req, res) => {
-    const { description, value, type } = req.body;
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
+  const { description, value, type } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
 
-    if (!token) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401);
 
-    const newTransactionSchema = joi.object({
-        description: joi.string().min(2).required(),
-        value: joi.number().required(),
-        type: joi.string().valid("income", "expense").required(),
-        token: joi.string().required(),
-    });
+  const newTransactionSchema = joi.object({
+    description: joi.string().min(2).required(),
+    value: joi.number().required(),
+    type: joi.string().valid("income", "expense").required(),
+    token: joi.string().required(),
+  });
 
-    const validation = newTransactionSchema.validate({
-        description,
-        value,
-        type,
-        token
-    });
+  const validation = newTransactionSchema.validate({
+    description,
+    value,
+    type,
+    token,
+  });
 
-    if (validation.error) return res.status(400).send(validation.error.message);
+  if (validation.error) return res.status(400).send(validation.error.message);
 
-    try {
-        const tokenExists = await db.collection(SESSIONS).findOne({ token });
+  try {
+    const tokenExists = await db.collection(SESSIONS).findOne({ token });
 
-        if (!tokenExists) return res.sendStatus(401);
+    if (!tokenExists) return res.sendStatus(401);
 
-        const lastSubtotal = await db.collection(REGISTRY).find({ userId: tokenExists.user_id }).sort({ $natural: -1 }).limit(1).toArray()
+    const today = dayjs().format("DD-MM");
 
-        const today = dayjs().format("DD-MM");
+    let newTransaction;
 
-        let newTransaction
+    if (type === "income") {
+      newTransaction = {
+        userId: tokenExists.user_id,
+        date: today,
+        description: description,
+        value: parseFloat(value).toFixed(2),
+        type: type,
 
-        if (type === "income") {
-            newTransaction = {
-                userId: tokenExists.user_id,
-                date: today,
-                description: description,
-                value: Number(value),
-                type: type,
-                subtotal: Number(lastSubtotal.length > 0 ? (Number(lastSubtotal[0].subtotal) + Number(value)).toFixed(2) : 0 + Number(value).toFixed(2))
-            };
-        } else if (type === "expense") {
-            newTransaction = {
-                userId: tokenExists.user_id,
-                date: today,
-                description: description,
-                value: Number(value),
-                type: type,
-                subtotal: Number(lastSubtotal.length > 0 ? (Number(lastSubtotal[0].subtotal) - Number(value)).toFixed(2) : 0 - Number(value).toFixed(2))
-            };
-        }
-
-        await db.collection(REGISTRY).insertOne(newTransaction);
-
-        return res.sendStatus(200);
-    } catch (error) {
-        res.sendStatus(500);
-        console.log(error)
+        //BREAK
+      };
+    } else if (type === "expense") {
+      newTransaction = {
+        userId: tokenExists.user_id,
+        date: today,
+        description: description,
+        value: parseFloat(value * -1).toFixed(2),
+        type: type,
+      };
     }
+
+    const userRegistry = await db
+      .collection(REGISTRY)
+      .find({ userId: tokenExists.user_id })
+      .toArray();
+
+    console.log("aqui quejo");
+    console.log(userRegistry);
+
+    const subtotal = {
+      userId: tokenExists.user_id,
+      subtotal: Number(newTransaction.value),
+    };
+
+    if (userRegistry.length === 0) {
+      await db.collection("subtotal").insertOne(subtotal);
+    }
+
+    if (userRegistry.length > 0) {
+      const userSubtotal = await db
+        .collection("subtotal")
+        .findOne({ userId: tokenExists.user_id });
+
+      await db
+        .collection("subtotal")
+        .updateOne(
+          { _id: ObjectId(userSubtotal._id) },
+          { $inc: { subtotal: Number(newTransaction.value) } }
+        );
+    }
+
+    await db.collection(REGISTRY).insertOne(newTransaction);
+
+    return res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(error);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.sendStatus(401);
+
+  const newTransactionSchema = joi.object({
+    id: joi.string().required(),
+    token: joi.string().required(),
+  });
+
+  const validation = newTransactionSchema.validate({
+    id,
+    token,
+  });
+
+  if (validation.error) return res.status(400).send(validation.error.message);
+
+  try {
+    const tokenExists = await db.collection(SESSIONS).findOne({ token });
+
+    if (!tokenExists) return res.sendStatus(401);
+
+    const deletedTransaction = await db
+      .collection(REGISTRY)
+      .findOne({ _id: ObjectId(id) });
+
+    const deletedValue = Number(deletedTransaction.value) * -1;
+
+    await db.collection(REGISTRY).deleteOne({ _id: ObjectId(id) });
+
+    await db
+      .collection("subtotal")
+      .updateOne(
+        { userId: ObjectId(tokenExists.user_id) },
+        { $inc: { subtotal: Number(deletedValue) } }
+      );
+
+    const userRegistry = await db
+      .collection(REGISTRY)
+      .find({ userId: tokenExists.user_id })
+      .toArray();
+
+    if (userRegistry.length === 0) return res.send([]).status(200);
+
+    const userSubtotal = await db
+      .collection("subtotal")
+      .findOne({ userId: tokenExists.user_id });
+
+    const userData = [[tokenExists.name], [...userRegistry], [userSubtotal]];
+
+    res.status(200).send(userData);
+
+    // await db.collection(REGISTRY).deleteOne({ _id: ObjectId(id)})
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(error);
+  }
 });
