c7ccb76bbe12a157d84d3baef0efee659ce6d8db
expeditus303 <ricardotonietto@gmail.com>
2023-10-26 11:34:33 -0300

    feat: set a fix port to server

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
