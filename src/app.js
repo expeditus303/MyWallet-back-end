991a7bf4f316676efbb93c61cfc4594bdd5feae0
expeditus303 <ricardotonietto@gmail.com>
2023-10-30 20:31:37 -0300

    feat: github actions for deploy on AWS

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT: ${PORT}!!`);
+  console.log(`Server listening on PORT => ${PORT}`);
 });
 
 app.use([ authRouter, transactionRouter ])
